078b3fcef016615120175178f27695ba5a0fff8b
evila <emanuele.villa@cern.ch>
2024-07-01 11:28:21 +0200

    cleanup, getting rid of old stuff, cleaned home folder

--- remove_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-jobs=["9869931", "9869932", "9869933", "9869934", "9869935", "9869936", "9869937", "9869938"]
-
-for job in $jobs
-do
-	echo "condor_rm $job"
-	eval condor_rm $job
-done
