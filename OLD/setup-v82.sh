bf1c884da78e35ed5a1374a06c1dba0c22eac89d
emanuele-villa <emanuele.villa@cern.ch>
2025-03-07 17:11:30 +0100

    cleanup, changed some scripts to new naming, added python folder, new sn spectra

--- OLD/setup-v82.sh
+++
@@ -1,8 +0,0 @@
-# load ups products
-#source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-
-export DUNESW_VERSION=v09_82_02d01; # this is what Klaudia used to test the workflow
-export DUNESW_QUALIFIER=e26:prof;
-echo 'Setting up dunesw version' $DUNESW_VERSION', qualifier' $DUNESW_QUALIFIER '...'
-setup dunesw $DUNESW_VERSION -q $DUNESW_QUALIFIER
-echo 'Done!'
