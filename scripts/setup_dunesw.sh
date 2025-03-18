e17246c3f02b5b10f54b8a52952e1afff693e7a7
emanuele-villa <villa.97@live.it>
2025-03-18 18:23:46 +0100

    made behavior more general using general setup script

---
+++ scripts/setup_dunesw.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+
+# $1 needs to be the full version in the form vXX_YY_ZZdAA
+# e.g. v10_04_05d00
+
+echo "Setting up dunesw $1"
+
+IFS='d' read -r -a array <<< "$1"
+version=${array[0]}
+extension=d${array[1]}
+
+# version="v10_04_05"
+# extension="d00" 
+export DUNESW_VERSION=$version$extension;
+qualifier="e26"
+specifier="prof"
+export DUNESW_QUALIFIER=${qualifier}:${specifier}
+
+source /afs/cern.ch/work/e/evilla/private/dune/dunesw/$DUNESW_VERSION/localProducts_larsoft_${version}_${qualifier}_${specifier}/setup # this will be the setup in your local installation
+THIS_PWD=$PWD
+cd $MRB_BUILDDIR
+mrbslp
+
+echo 'Setting up dunesw version' $DUNESW_VERSION', qualifier' $DUNESW_QUALIFIER '...'
+setup dunesw $DUNESW_VERSION -q $DUNESW_QUALIFIER
+
+echo 'Done!'
+
+cd $THIS_PWD # go back where we were
